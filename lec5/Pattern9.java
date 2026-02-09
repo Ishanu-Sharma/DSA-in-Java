4bf15383413532181efd532bdf27777b6488fc53
Ishanu Sharma <ishanushrm@gmail.com>
2026-02-09 14:25:22 +0530

    Add files via upload

--- lec5/Pattern9.java
+++ lec5/Pattern9.java
@@ -3,22 +3,26 @@ public class Pattern9{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        int star = x;
-        int space = 0;
-        for(int row=1; row<=x; row++){
-            for(int j=1; j<=star; j++){
+        for(int row=1; row<=(2*x)-1; row++){
                 System.out.print("* ");
             }
-            for(int i = 1; i<=space; i++){
-                System.out.print("  ");
-            }
+            System.out.println();
+        int space = 1;
+            for(int i = x-1; i>=1; i--)
+            {
+                for(int j = 1 ; j<=i; j++){
+                    
+                System.out.print("* ");
+                }
+                for(int k = 1; k<=space; k++){
+                    System.out.print("  ");
+                }
             
-            for(int k=1; k<=star; k++){
+            for(int l=1; l<=i; l++){
                 System.out.print("* ");
             }
             System.out.println();
             space+=2;
-            star--;
         }
     }
 }
